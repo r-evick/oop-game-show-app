28004bc23d0b7efd13609c070c93f813ba49d696
r-evick <r.evick@hotmail.com>
2021-06-27 02:43:40 -0400

    random phrase
    added phrases and method for selecting random phrase

--- js/Game.js
+++ js/Game.js
@@ -6,8 +6,39 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
-        this.activePhrase = 'null';
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
     }
-}
 
+/** 
+* Creates phrases for use in game 
+* @return {array} An array of phrases that could be used in the game 
+*/ 
+
+createPhrases() {
+    const phrase = [
+        new Phrase('Theres no place like home'),
+        new Phrase('Youre gonna need a bigger boat'),
+        new Phrase('Houston we have a problem'),
+        new Phrase('Why so serious'),
+        new Phrase('Say hello to my little friend'),
+    ];
+
+    return phrase;
+};
+
+/** 
+* Selects random phrase from phrases property 
+* @return {Object} Phrase object chosen to be used 
+*/ 
+
+getRandomPhrase() {
+    const randomPhraseFunction = Math.floor(Math.random() * this.phrases.length);
+    const randomPhrase = this.phrases[randomPhraseFunction];
+
+    return randomPhrase;
+};
+
+
+
+};
--- js/app.js
+++ js/app.js
@@ -3,7 +3,21 @@
  * app.js */
 
 
-const phrase = new Phrase('Life is like a box of chocolates'); 
-console.log(`Phrase - phrase: ${phrase.phrase}`);
+// const phrase = new Phrase('Life is like a box of chocolates'); 
+// console.log(`Phrase - phrase: ${phrase.phrase}`);
 
+// const game = new Game(); 
+// game.phrases.forEach((phrase, index) => { 
+// console.log(`Phrase ${index} - phrase: ${phrase.phrase}`); 
+// });
 
+// const logPhrase = (phrase) => { 
+//     console.log(`Phrase - phrase: `, phrase.phrase); 
+//     }; 
+//     const game = new Game(); 
+//     logPhrase(game.getRandomPhrase()); 
+//     logPhrase(game.getRandomPhrase()); 
+//     logPhrase(game.getRandomPhrase()); 
+//     logPhrase(game.getRandomPhrase()); 
+//     logPhrase(game.getRandomPhrase());
+    

5e1dd9d401fe46eaa85d0c7f926dcc4147ce0726
r-evick <r.evick@hotmail.com>
2021-07-01 01:07:06 -0400

    added methods
    added checkForWin, removeLife, and gameOver methods to Game class.
    
    added checkLetter and showMatchedLetter methods to Phrase class.

--- js/Game.js
+++ js/Game.js
@@ -46,7 +46,6 @@ getRandomPhrase() {
 * Begins game by selecting a random phrase and displaying it to user 
 */ 
 
-
 startGame() {
     const overlay = document.getElementById('overlay');
     
@@ -54,6 +53,62 @@ startGame() {
     this.activePhrase = this.getRandomPhrase();
     this.activePhrase.addPhraseToDisplay(); 
 };
+
+
+handleInteraction() {
+
+
+};
+
+/** 
+* Checks for winning move 
+* @return {boolean} True if game has been won, false if game wasn't won 
+*/ 
+
+checkForWin() {
+    const hiddenLetters = document.querySelectorAll('.hide');
+
+    return hiddenLetters.length === 0;
+};
+
+
+/** 
+* Increases the value of the missed property 
+* Removes a life from the scoreboard 
+* Checks if player has remaining lives and ends game if player is out 
+*/ 
+
+removeLife() {
+    const liveHeart = document.querySelectorAll('img');
+    liveHeart[this.missed].src = 'images/lostHeart.png';
+    this.missed++;
+
+    if (this.missed === 5) {
+        this.gameOver(false);
+    }
+};
+
+
+/** 
+* Displays game over message 
+* @param {boolean} gameWon - Whether or not the user won the game 
+*/ 
+
+gameOver(gameWon) {
+    let message = document.getElementById('game-over-message');
+    overlay.style.display = '';
+
+    if  (gameWon) {
+        overlay.className = 'win';
+        message.textContent = `Congratulations, you won!`;
+    } else {
+        overlay.className = 'lose';
+        message.textContent = `Sorry, you did not win. Better luck next time!`;
+    }    
+    
+};
+
+
 
 
 };
--- js/Phrase.js
+++ js/Phrase.js
@@ -30,10 +30,36 @@ addPhraseToDisplay() {
           li.classList.add(this.phrase[i]);
           ul.appendChild(li); 
       }
-    }
-    
-  
+    }  
 };
+
+
+/** 
+* Checks if passed letter is in phrase 
+* @param (string) letter - Letter to check 
+*/ 
+
+checkLetter(letter) {
+    if (this.phrase.includes(letter)) {  //includes() method used from https://www.w3schools.com/Jsref/jsref_includes.asp
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+/** 
+* Displays passed letter on screen after a match is found * @param (string) letter - Letter to display 
+*/ 
+showMatchedLetter(letter) {
+    const matchedLetter = document.getElementsByClassName(letter);
+
+    for (let i = 0; i < matchedLetter.length; i++) {
+        matchedLetter[i].classList.remove('hide');
+        matchedLetter[i].classList.add('show');
+    }
+};
+
 
 
 };
